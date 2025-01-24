af9ec7142032a75ef40458353ed8239716dab975
Maddie Nevans <maddieelizabethtaylor@gmail.com>
2025-01-24 16:26:58 -0500

    Initial Commit

---
+++ Snake/SnakeApp.swift
@@ -0,0 +1,32 @@
+//
+//  SnakeApp.swift
+//  Snake
+//
+//  Created by Maddie Nevans on 1/24/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SnakeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
